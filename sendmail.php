d27104385c1533afe363c7e7aebfabb7fb9999cb
juniormalk <juniormalk@gmail.com>
2023-12-21 14:12:33 -0300

    asd

--- sendmail.php
+++ sendmail.php
@@ -24,8 +24,8 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com;';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'juniormalk';                 // SMTP username
-$mail->Password = 'Aguiav0@alto';                           // SMTP password
-// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Password = 'Aguiav0@alto@alto';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;    
 
 $mail->setFrom($email); 
